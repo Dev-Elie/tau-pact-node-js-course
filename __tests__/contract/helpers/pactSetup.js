a97b5a31e8dbbe1edcb56e47584c396d8c072541
Dev-Elie <eochieng9448@gmail.com>
2022-08-28 18:01:39 +0300

    test: added consumer tests

--- __tests__/contract/helpers/pactSetup.js
+++ __tests__/contract/helpers/pactSetup.js
@@ -5,7 +5,7 @@ const path = require("path")
 const Pact = require("@pact-foundation/pact")
 
 global.port = 8081
-global.provider = Pact({
+global.provider = new Pact({
     port: global.port,
     log: path.resolve(process.cwd(), "__tests__/contract/logs", "pact-logs.log"),
     dir: path.resolve(process.cwd(), "__tests__/contract/pacts"),
